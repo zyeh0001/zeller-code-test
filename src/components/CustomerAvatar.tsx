e809d2c6e2dd9b926404ae7871400e8322b40307
zyeh0001 <69131379+zyeh0001@users.noreply.github.com>
2023-10-15 15:26:05 +1100

    mock graphql query request

--- src/components/CustomerAvatar.tsx
+++ src/components/CustomerAvatar.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Avatar } from "@mui/material";
 import { Customer } from "../interfaces/index";
 import Tooltip from "@mui/material/Tooltip";
-import { LIGHT_BLUE, BLUE } from "../styles/colors";
+import { LIGHT_BLUE, BLUE, WHITE } from "../styles/colors";
 import { Font } from "../styles/fonts";
+import { capitalizeFirstLetter } from "utils/textUtils";
+import { Card, Typography } from "@mui/material";
+import styled from "@emotion/styled";
 
 interface CustomerAvatarProps {
   customer: Customer;
@@ -11,7 +14,10 @@ interface CustomerAvatarProps {
 
 const CustomerAvatar = ({ customer }: CustomerAvatarProps) => {
   return (
-    <Tooltip title={customer.email}>
+    <Tooltip
+      sx={{ background: WHITE }}
+      title={<CustomerDetailInfo customer={customer} />}
+    >
       <Avatar {...stringAvatar(customer.name)} variant="rounded" />
     </Tooltip>
   );
@@ -27,5 +33,31 @@ function stringAvatar(name: string) {
     children: `${name.split(" ")[0][0]}`,
   };
 }
+
+type CustomerDetailInfoProps = { customer: Customer };
+const CustomerDetailInfo = ({ customer }: CustomerDetailInfoProps) => {
+  return (
+    <CustomerDetailContainer>
+      <Avatar
+        alt="George bush"
+        src="https://www.w3schools.com/howto/img_avatar.png"
+      />
+      <br />
+      <Typography sx={{ fontSize: Font.large }}>{customer.name}</Typography>
+      <Typography sx={{ fontSize: Font.regular }}>{customer.email}</Typography>
+      <Typography sx={{ fontSize: Font.regular }}>
+        {capitalizeFirstLetter(customer.role)}
+      </Typography>
+    </CustomerDetailContainer>
+  );
+};
+
+const CustomerDetailContainer = styled(Card)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px 40px",
+  border: "none",
+}));
 
 export default CustomerAvatar;
